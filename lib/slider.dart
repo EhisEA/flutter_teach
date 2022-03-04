3ce2ed322b2319d4facb5d61c9d2bcb8dd2a2c82
EhisEA <emmanueleayemere@gmail.com>
2022-03-04 20:26:24 +0100

    slider 2

--- lib/slider.dart
+++ lib/slider.dart
@@ -8,6 +8,8 @@ class SliderRan extends StatefulWidget {
 }
 
 class _SliderRanState extends State<SliderRan> {
+  TextEditingController minTextController = TextEditingController(text: "20");
+  TextEditingController maxTextController = TextEditingController(text: "89");
   RangeValues rang = RangeValues(20, 89);
   @override
   Widget build(BuildContext context) {
@@ -22,9 +24,40 @@ class _SliderRanState extends State<SliderRan> {
             onChanged: (RangeValues value) {
               setState(() {
                 rang = value;
+                minTextController.text = value.start.toString();
+                maxTextController.text = value.end.toString();
               });
             },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                showText("From", minTextController),
+                const Spacer(flex: 1),
+                showText("To", maxTextController),
+              ],
+            ),
           )
+        ],
+      ),
+    );
+  }
+
+  Widget showText(String title, TextEditingController controller) {
+    return Expanded(
+      flex: 3,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title),
+          TextField(
+            controller: controller,
+            decoration: const InputDecoration(
+              enabledBorder: OutlineInputBorder(),
+              focusedBorder: OutlineInputBorder(),
+            ),
+          ),
         ],
       ),
     );
